be27edbf966e731b2bde480f4f8445e805124b9c
bigbossoh <michel.bossoh@outlook.fr>
2021-01-22 16:25:34 +0000

    Installer angular Material projet

---
+++ frontendfirst/src/app/admin/admin.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { OverviewComponent } from './components/overview/overview.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+@NgModule({
+  declarations: [OverviewComponent, LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
